189c51066520151df2910ba2acf2a19ab0cf2dec
avi <avi.vajpeyi@gmail.com>
2023-10-16 18:21:58 +1300

    add waveform-generator template

---
+++ src/pywavelet/waveform_generator/waveform_generator.py
@@ -0,0 +1,18 @@
+class Waveform:
+
+    def __init__(self, table_file):
+        self.table_file = table_file
+
+
+    def __call__(self, **params):
+        """Call the waveform generator (using the lookup table) with the given parameters."""
+        pass
+
+    def
+
+    def __build_table(self):
+        pass
+
+    def __load_table(self):
+        pass
+
