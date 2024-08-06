e676e65d746be32d2b7a58349beece9512f4835e
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-08-07 11:50:13 +1200

    fix version test

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,7 +1,3 @@
-import re
-
-import git
-
 import pywavelet
 
 
@@ -13,13 +9,3 @@ def test_version():
         pywavelet.__version__, str
     ), f"{pywavelet.__version__} is not a string"
     assert pywavelet.__version__ > "0.0.0", f"{pywavelet.__version__} <= 0.0.0"
-
-    # Get the latest git tag
-    repo = git.Repo(search_parent_directories=True)
-    git_tag = repo.git.describe(tags=True, abbrev=0)
-
-    # Check that the version matches the last tag
-    re_match = re.match(r"v(\d+\.\d+\.\d+)", git_tag)
-    assert (
-        re_match.group(1) == pywavelet.__version__
-    ), f"{re_match.group(1)} != {pywavelet.__version__}"
