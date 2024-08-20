fdaf7d9ad6e2abe16bfd820cbea380dca9cb021b
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-08-21 00:39:10 +1200

    remove CBC waveform

--- tests/utils.py
+++ tests/utils.py
@@ -2,26 +2,10 @@ from typing import List, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
-# from pycbc.conversions import mass1_from_mchirp_q, mass2_from_mchirp_q
-# from pycbc.waveform import get_td_waveform
-from scipy.signal import chirp, spectrogram
+from scipy.signal import chirp
 from scipy.signal.windows import tukey
 
 from pywavelet.transforms.types import TimeAxis, TimeSeries
-
-
-def cbc_waveform(mc, q=1, delta_t=1.0 / 4096, f_lower=20):
-    m1 = mass1_from_mchirp_q(mc, q)
-    m2 = mass2_from_mchirp_q(mc, q)
-    hp, hc = get_td_waveform(
-        approximant="IMRPhenomD",
-        mass1=m1,
-        mass2=m2,
-        delta_t=delta_t,
-        f_lower=f_lower,
-    )
-    data = TimeSeries(hp.data, time=TimeAxis(hp.sample_times.data))
-    return data
 
 
 def waveform_fft(
