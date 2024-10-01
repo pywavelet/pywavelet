8697db0dcee36648c7d4b8062ae57b8d56cb344f
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-10-01 16:57:38 +1300

    pytest fixes

--- tests/utils.py
+++ tests/utils.py
@@ -8,40 +8,18 @@ from scipy.signal.windows import tukey
 from pywavelet.transforms.types import TimeSeries, FrequencySeries
 
 
-def waveform_fft(
-    t,
-    waveform,
-):
-    N = len(waveform)
-    taper = tukey(N, 0.1)
-    waveform_w_pad = zero_pad(waveform * taper)
-    waveform_f = np.fft.rfft(waveform_w_pad)[1:]
-    n_t = len(zero_pad(t))
-    delta_t = t[1] - t[0]
-    freq = np.fft.rfftfreq(n_t, delta_t)[1:]
-    return freq, waveform_f
-
-
-def zero_pad(data):
-    N = len(data)
-    pow_2 = np.ceil(np.log2(N))
-    return np.pad(data, (0, int((2**pow_2) - N)), "constant")
-
-
-def symlog_bins(data, lin_thresh=None, log_base=10, num_lin=5, num_log=10):
+def __symlog_bins(data, lin_thresh=None, log_base=10, num_lin=5, num_log=10):
     if lin_thresh is None:
-        lin_thresh= 0.05 * np.std(data)
+        lin_thresh = 0.05 * np.std(data)
     lin_bins = np.linspace(-lin_thresh, lin_thresh, num_lin)
     log_bins_pos = np.logspace(np.log10(lin_thresh), np.log10(np.max(np.abs(data))), num=num_log, base=log_base)
     log_bins_neg = -np.logspace(np.log10(lin_thresh), np.log10(np.max(np.abs(data))), num=num_log, base=log_base)[::-1]
     bins = np.concatenate([log_bins_neg, lin_bins, log_bins_pos])
-
-    bins = np.concatenate([log_bins_neg, lin_bins[1:-1], log_bins_pos])
     bins = np.sort(np.unique(bins))
     return bins, lin_thresh
 
 
-def plot_residuals(residuals, ax:plt.Axes, symlog=True):
+def plot_residuals(residuals, ax: plt.Axes, symlog=True):
     mean, std = np.mean(residuals), np.std(residuals)
     ax.text(
         0.05,
@@ -51,13 +29,10 @@ def plot_residuals(residuals, ax:plt.Axes, symlog=True):
         fontsize=12,
         verticalalignment="top",
     )
-
-
     num_nans = np.sum(np.isnan(residuals))
     assert num_nans == 0, f"Found {num_nans} NaNs in residuals."
-
     if symlog:
-        bins, lin_thresh = symlog_bins(residuals)
+        bins, lin_thresh = __symlog_bins(residuals)
         ax.hist(residuals, bins=bins, density=False)
         ax.set_xscale("symlog", linthresh=lin_thresh)
     else:
@@ -67,20 +42,19 @@ def plot_residuals(residuals, ax:plt.Axes, symlog=True):
 
 
 def generate_chirp_time_domain_signal(
-    t: np.ndarray, freq_range: List[float]
+        t: np.ndarray, freq_range: List[float]
 ) -> TimeSeries:
     fs = 1 / (t[1] - t[0])
     nyquist = fs / 2
     fmax = max(freq_range)
     assert (
-        fmax < nyquist
+            fmax < nyquist
     ), f"f_max [{fmax:.2f} Hz] must be less than f_nyquist [{nyquist:2f} Hz]."
 
     y = chirp(
         t, f0=freq_range[0], f1=freq_range[1], t1=t[-1], method="quadratic"
     )
     return TimeSeries(data=y, time=t)
-
 
 
 def __zero_pad(data):
@@ -90,7 +64,7 @@ def __zero_pad(data):
     """
     N = len(data)
     pow_2 = np.ceil(np.log2(N))
-    return np.pad(data, (0, int((2**pow_2) - N)), "constant")
+    return np.pad(data, (0, int((2 ** pow_2) - N)), "constant")
 
 
 def generate_sine_time_domain_signal(ts, n, f_true=10):
@@ -99,8 +73,9 @@ def generate_sine_time_domain_signal(ts, n, f_true=10):
     h_signal = __zero_pad(h_signal * window)
     return TimeSeries(h_signal, time=ts)
 
+
 def generate_sine_freq_domain_signal(fs, f_true=10):
-    freq = np.fft.rfftfreq(fs, d=1/fs)
+    freq = np.fft.rfftfreq(fs, d=1 / fs)
     hf = np.zeros_like(freq)
     # closest frequency bin to f_true
     idx = np.argmin(np.abs(freq - f_true))
