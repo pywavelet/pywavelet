b124dc75c13540a946b665e0bbb70de43ffe16fd
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-02-03 10:51:25 +1300

    more plotting for phi

--- tests/test_phi.py
+++ tests/test_phi.py
@@ -4,19 +4,7 @@ import numpy as np
 from pywavelet.transforms import phi_vec, phitilde_vec, phitilde_vec_norm
 
 
-def test_phi(plot_dir):
-    d, q = 4.0, 16
-    Nf, Nt = 3, 64
-    dt = 2
-
-    delT = Nf * dt
-    delF = 1.0 / (2 * Nf * dt)
-
-    phi = phi_vec(Nf=Nf, d=d, q=q) / delT
-    t = np.linspace(-q, q, len(phi))
-    f = np.linspace(-1, 1, 1000)
-    phitilde = phitilde_vec(f, Nf=Nf, d=d) / delF
-
+def _plot(t, phi, f, phitilde, fname):
     fig, ax = plt.subplots(
         2,
         1,
@@ -30,4 +18,21 @@ def test_phi(plot_dir):
     ax[1].set_ylabel("$\\tilde{\phi(f)}/\Delta t$")
     ax[1].set_xlabel("$f/\Delta F$")
     plt.tight_layout()
-    plt.savefig(f"{plot_dir}/phi.png")
+    plt.savefig(fname)
+
+
+def test_phi(plot_dir):
+    d, q = 4.0, 16
+    Nf, Nt = 3, 64
+    dt = 2
+
+    delT = Nf * dt
+    delF = 1.0 / (2 * Nf * dt)
+
+    phi = phi_vec(Nf=Nf, d=d, q=q) / delT
+    t = np.linspace(-q, q, len(phi))
+    f = np.linspace(-1, 1, 1000)
+    phitilde = phitilde_vec(f, Nf=Nf, d=d) / delF
+
+    _plot(t, phi, f, phitilde, plot_dir / "phi.png")
+
