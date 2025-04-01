754bc0fc265ce85002cea828ce02a59574c97fca
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-04-01 14:36:40 +1300

    add more examples

--- src/pywavelet/types/plotting.py
+++ src/pywavelet/types/plotting.py
@@ -1,371 +1,172 @@
-import warnings
-from typing import Optional, Tuple, Union
+import h5py
+import numpy as np
+
+from lisatools.sensitivity import get_sensitivity
+
+from pywavelet.types import FrequencySeries, Wavelet
+from dataclasses import dataclass
+from typing import Dict
+import scipy
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from matplotlib.colors import LogNorm
+from matplotlib.ticker import ScalarFormatter
 
 import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib.colors import LogNorm, TwoSlopeNorm
-from scipy.interpolate import interp1d
-from scipy.signal import savgol_filter, spectrogram
 
-MIN_S = 60
-HOUR_S = 60 * MIN_S
-DAY_S = 24 * HOUR_S
+# Set the desired RC parameters
+rc_params = {
+    'xtick.direction': 'in',  # Mirrored ticks (in and out)
+    'ytick.direction': 'in',
+    'xtick.top': True,  # Show ticks on the top spine
+    'ytick.right': True,  # Show ticks on the right spine
+    'xtick.major.size': 6,  # Length of major ticks
+    'ytick.major.size': 6,
+    'xtick.minor.size': 4,  # Length of minor ticks
+    'ytick.minor.size': 4,
+    'xtick.major.pad': 4,  # Padding between tick and label
+    'ytick.major.pad': 4,
+    'xtick.minor.pad': 4,
+    'ytick.minor.pad': 4,
+    'font.size': 14,  # Overall font size
+    'axes.labelsize': 16,  # Font size of axis labels
+    'axes.titlesize': 18,  # Font size of plot title
+    'xtick.labelsize': 12,  # Font size of x-axis tick labels
+    'ytick.labelsize': 12,  # Font size of y-axis tick labels
+    'xtick.major.width': 2,  # Thickness of major ticks
+    'ytick.major.width': 2,  # Thickness of major ticks
+    'xtick.minor.width': 1,  # Thickness of minor ticks
+    'ytick.minor.width': 1,  # Thickness of minor ticks
+    'lines.linewidth': 3,  # Default linewidth for lines in plots
+    'patch.linewidth': 4,  # Default linewidth for patches (e.g., rectangles, circles)
+    'axes.linewidth': 2  # Default linewidth for the axes spines
 
+}
 
-def plot_wavelet_trend(
-    wavelet_data: np.ndarray,
-    time_grid: np.ndarray,
-    freq_grid: np.ndarray,
-    ax: Optional[plt.Axes] = None,
-    freq_scale: str = "linear",
-    freq_range: Optional[Tuple[float, float]] = None,
-    color: str = "black",
-):
-    x = time_grid
-    y = __get_smoothed_y(x, np.abs(wavelet_data), freq_grid)
-    if ax == None:
-        fig, ax = plt.subplots()
-    ax.plot(x, y, color=color)
+# Apply the RC parameters globally
+plt.rcParams.update(rc_params)
 
-    # Configure axes scales
-    ax.set_yscale(freq_scale)
-    _fmt_time_axis(time_grid, ax)
-    ax.set_ylabel("Frequency [Hz]")
-
-    # Set frequency range if specified
-    freq_range = freq_range or (freq_grid[0], freq_grid[-1])
-    ax.set_ylim(freq_range)
-
-
-def __get_smoothed_y(x, z, y_grid):
-    Nf, Nt = z.shape
-    y = np.zeros(Nt)
-    dy = np.diff(y_grid)[0]
-    for i in range(Nt):
-        # if all values are nan, set to nan
-        if np.all(np.isnan(z[:, i])):
-            y[i] = np.nan
-        else:
-            y[i] = y_grid[np.nanargmax(z[:, i])]
-
-    if not np.isnan(y).all():
-        # Interpolate to fill NaNs in y before smoothing
-        nan_mask = ~np.isnan(y)
-        if np.isnan(y).any():
-            interpolator = interp1d(
-                x[nan_mask],
-                y[nan_mask],
-                kind="cubic",
-                bounds_error=False,
-                fill_value="extrapolate",
-            )
-            y = interpolator(x)  # Fill NaNs with interpolated values
-
-        # Smooth the curve
-        window_length = min(51, len(y) - 1 if len(y) % 2 == 0 else len(y))
-        y = savgol_filter(y, window_length, 3)
-        y[~nan_mask] = np.nan
-    return y
+COLORS = dict(
+    mbh="tab:purple",
+    emri="tab:red",
+    gb="tab:cyan"
+)
 
 
-def plot_wavelet_grid(
-    wavelet_data: np.ndarray,
-    time_grid: np.ndarray,
-    freq_grid: np.ndarray,
-    ax: Optional[plt.Axes] = None,
-    zscale: str = "linear",
-    freq_scale: str = "linear",
-    absolute: bool = False,
-    freq_range: Optional[Tuple[float, float]] = None,
-    show_colorbar: bool = True,
-    cmap: Optional[str] = None,
-    norm: Optional[Union[LogNorm, TwoSlopeNorm]] = None,
-    cbar_label: Optional[str] = None,
-    nan_color: Optional[str] = "black",
-    detailed_axes: bool = False,
-    show_gridinfo: bool = True,
-    txtbox_kwargs: dict = {},
-    trend_color: Optional[str] = None,
-    whiten_by: Optional[np.ndarray] = None,
-    **kwargs,
-) -> Tuple[plt.Figure, plt.Axes]:
+@dataclass
+class PlotData:
+    freqseries: FrequencySeries
+    wavelet: Wavelet
+    label: str
+
+
+def load_data() -> Dict[str, PlotData]:
     """
-    Plot a 2D grid of wavelet coefficients.
-
-    Parameters
-    ----------
-    wavelet_data : np.ndarray
-        A 2D array containing the wavelet coefficients with shape (Nf, Nt),
-        where Nf is the number of frequency bins and Nt is the number of time bins.
-
-    time_grid : np.ndarray, optional
-        1D array of time values corresponding to the time bins. If None, uses np.arange(Nt).
-
-    freq_grid : np.ndarray, optional
-        1D array of frequency values corresponding to the frequency bins. If None, uses np.arange(Nf).
-
-    ax : plt.Axes, optional
-        Matplotlib Axes object to plot on. If None, creates a new figure and axes.
-
-    zscale : str, optional
-        Scale for the color mapping. Options are 'linear' or 'log'. Default is 'linear'.
-
-    freq_scale : str, optional
-        Scale for the frequency axis. Options are 'linear' or 'log'. Default is 'linear'.
-
-    absolute : bool, optional
-        If True, plots the absolute value of the wavelet coefficients. Default is False.
-
-    freq_range : tuple of float, optional
-        Tuple specifying the (min, max) frequency range to display. If None, displays the full range.
-
-    show_colorbar : bool, optional
-        If True, displays a colorbar next to the plot. Default is True.
-
-    cmap : str, optional
-        Colormap to use for the plot. If None, uses 'viridis' for absolute values or 'bwr' for signed values.
-
-    norm : matplotlib.colors.Normalize, optional
-        Normalization instance to scale data values. If None, a suitable normalization is chosen based on `zscale`.
-
-    cbar_label : str, optional
-        Label for the colorbar. If None, a default label is used based on the `absolute` parameter.
-
-    nan_color : str, optional
-        Color to use for NaN values. Default is 'black'.
-
-    trend_color : bool, optional
-        Color to use for the trend line. Not shown if None.
-
-    **kwargs
-        Additional keyword arguments passed to `ax.imshow()`.
-
-    Returns
-    -------
-    Tuple[plt.Figure, plt.Axes]
-        The figure and axes objects of the plot.
-
-    Raises
-    ------
-    ValueError
-        If the dimensions of `wavelet_data` do not match the lengths of `freq_grid` and `time_grid`.
+    Load data from HDF5 files.
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: (frequency array, waveform)
     """
+    # Load the data from the HDF5 file
+    keys = ['emri', 'gb', 'mbh']
+    data = {}
+    with h5py.File('data.h5', 'r') as f:
+        for key in keys:
+            freqseries = FrequencySeries(f[key]['hf'][:], f[key]['freq'][:])
+            # if key == 'gb':
+            #     ts = freqseries.to_timeseries()
+            #     ts = ts * scipy.signal.windows.tukey(len(ts), alpha=0.1)
+            #     freqseries = ts.to_frequencyseries()
 
-    # Determine the dimensions of the data
-    Nf, Nt = wavelet_data.shape
+            wdm = freqseries.to_wavelet(Nf=1024)
+            data[key] = PlotData(freqseries=freqseries, wavelet=wdm, label=key.upper())
+    return data
 
-    # Validate the dimensions
-    if (Nf, Nt) != (len(freq_grid), len(time_grid)):
-        raise ValueError(
-            f"Wavelet shape {Nf, Nt} does not match provided grids {(len(freq_grid), len(time_grid))}."
+
+def generate_plot(data: Dict[str, PlotData]):
+    # Create figure and gridspec
+    fig = plt.figure(figsize=(10, 8))
+    gs = gridspec.GridSpec(2, 3, figure=fig, height_ratios=[1, 1])  # 2 rows, 3 columns
+
+    # Create subplots
+    ax0 = fig.add_subplot(gs[0, :])  # Row 1: spans all 3 columns
+    ax1 = fig.add_subplot(gs[1, 0])  # Row 2, Column 1
+    ax2 = fig.add_subplot(gs[1, 1])  # Row 2, Column 2
+    ax3 = fig.add_subplot(gs[1, 2])  # Row 2, Column 3
+    axs = [ax1, ax2, ax3]
+
+    # rearrange data [emri, mbh, gb]
+    data = {k: data[k] for k in ['emri', 'mbh', 'gb']}
+
+    ### Plot characteristic strain
+    f_fixed = np.logspace(-4, -1, 1000)
+    sensitivity = get_sensitivity(f_fixed, sens_fn="LISASens", return_type="char_strain")
+    for k, d in data.items():
+        f, hf = d.freqseries.freq, d.freqseries.data
+        zorder=-10 if k=='emri' else 10
+        ax0.loglog(f[1:], (np.abs(hf) * f)[1:], label=f'{d.label}', color=COLORS[k], zorder=zorder)
+    ax0.loglog(f_fixed, sensitivity, 'k--', label='LISA Sensitivity', zorder=-5)
+    ax0.legend(frameon=False)
+    ax0.set_xlim(1e-4, 1e-1)
+    ax0.set_ylim(bottom=10 ** - 22, top=10 ** -18)
+    # reduce padding for xlabel
+    ax0.set_xlabel(r'$f$ [Hz]', labelpad=-5)
+    ax0.set_ylabel(r"Characteristic Strain")
+
+    ### Plot WDMs (share colorbar)
+    wdm_range = (np.inf, -np.inf)
+    for d in data.values():
+        wdm = np.abs(d.wavelet.data)
+        wdm_range = (min(wdm_range[0], np.min(wdm)), max(wdm_range[1], np.max(wdm)))
+    # log_norm = LogNorm(vmin=wdm_range[0], vmax=wdm_range[1])
+    log_norm = LogNorm(vmin=10 ** -31, vmax=10 ** -19)
+
+    # set text color = white
+
+
+
+    for i, (k, d) in enumerate(data.items()):
+        kwgs = dict(
+            absolute=True, zscale='log', cmap='inferno',
+            ax=axs[i], norm=log_norm,
+            show_gridinfo=False,
+            show_colorbar=False,
         )
-
-    # Prepare the data for plotting
-    z = wavelet_data.copy()
-    if whiten_by is not None:
-        z = z / whiten_by
-    if absolute:
-        z = np.abs(z)
-
-    # Determine normalization and colormap
-    if norm is None:
-        try:
-            if np.all(np.isnan(z)):
-                raise ValueError("All wavelet data is NaN.")
-            if zscale == "log":
-                vmin = np.nanmin(z[z > 0])
-                vmax = np.nanmax(z[z < np.inf])
-                if vmin > vmax:
-                    raise ValueError("vmin > vmax... something wrong")
-                norm = LogNorm(vmin=vmin, vmax=vmax)
-            elif not absolute:
-                vmin, vmax = np.nanmin(z), np.nanmax(z)
-                vcenter = 0.0
-                if vmin > vmax:
-                    raise ValueError("vmin > vmax... something wrong")
-
-                norm = TwoSlopeNorm(vmin=vmin, vcenter=vcenter, vmax=vmax)
-            else:
-                norm = None  # Default linear scaling
-        except Exception as e:
-            warnings.warn(
-                f"Error in determining normalization: {e}. Using default linear scaling."
-            )
-            norm = None
-
-    if cmap is None:
-        cmap = "viridis" if absolute else "bwr"
-        cmap = plt.get_cmap(cmap)
-        cmap.set_bad(color=nan_color)
-
-    # Set up the plot
-    if ax is None:
-        fig, ax = plt.subplots()
-    else:
-        fig = ax.get_figure()
-
-    # Plot the data
-    im = ax.imshow(
-        z,
-        aspect="auto",
-        extent=[time_grid[0], time_grid[-1], freq_grid[0], freq_grid[-1]],
-        origin="lower",
-        cmap=cmap,
-        norm=norm,
-        interpolation="nearest",
-        **kwargs,
-    )
-    if trend_color is not None:
-        plot_wavelet_trend(
-            wavelet_data,
-            time_grid,
-            freq_grid,
-            ax,
-            color=trend_color,
-            freq_range=freq_range,
-            freq_scale=freq_scale,
-        )
-
-    # Add colorbar if requested
-    if show_colorbar:
-        cbar = fig.colorbar(im, ax=ax)
-        if cbar_label is None:
-            cbar_label = (
-                "Absolute Wavelet Amplitude"
-                if absolute
-                else "Wavelet Amplitude"
-            )
-        cbar.set_label(cbar_label)
-
-    # Configure axes scales
-    ax.set_yscale(freq_scale)
-    _fmt_time_axis(time_grid, ax)
-    ax.set_ylabel("Frequency [Hz]")
-
-    # Set frequency range if specified
-    freq_range = freq_range or (freq_grid[0], freq_grid[-1])
-    ax.set_ylim(freq_range)
-
-    if detailed_axes:
-        ax.set_xlabel(r"Time Bins [$\Delta T$=" + f"{1 / Nt:.4f}s, Nt={Nt}]")
-        ax.set_ylabel(r"Freq Bins [$\Delta F$=" + f"{1 / Nf:.4f}Hz, Nf={Nf}]")
-
-    label = kwargs.get("label", "")
-    NfNt_label = f"{Nf}x{Nt}" if show_gridinfo else ""
-    txt = f"{label}\n{NfNt_label}" if label else NfNt_label
-    if txt:
-        txtbox_kwargs.setdefault("boxstyle", "round")
-        txtbox_kwargs.setdefault("facecolor", "white")
-        txtbox_kwargs.setdefault("alpha", 0.2)
-        ax.text(
+        if i == 2:
+            kwgs['show_colorbar'] = True
+        d.wavelet.plot(**kwgs)
+        axs[i].text(
             0.05,
             0.95,
-            txt,
-            transform=ax.transAxes,
-            fontsize=14,
+            d.label,
+            color=COLORS[k],
+            transform=axs[i].transAxes,
             verticalalignment="top",
-            bbox=txtbox_kwargs,
+            # extra bold font
+            fontweight='bold',
         )
 
-    # Adjust layout
-    fig.tight_layout()
+    ax1.set_ylabel(r"$f$ [Hz]")
+    ax2.set_ylabel('')
+    ax3.set_ylabel('')
+    ax1.set_ylim(top=0.02)
 
-    return fig, ax
+    cbar = plt.gca().images[-1].colorbar
 
+    # cbar = ax3.collections[0].colorbar
+    cbar.set_label(r'')
+    cbar.ax.yaxis.label.set(rotation=0, ha='right', va='bottom')
+    cbar.ax.yaxis.set_tick_params(rotation=0)
 
-def plot_freqseries(
-    data: np.ndarray,
-    freq: np.ndarray,
-    nyquist_frequency: float,
-    ax=None,
-    **kwargs,
-):
-    if ax == None:
-        fig, ax = plt.subplots()
-    ax.plot(freq, data, **kwargs)
-    ax.set_xlabel("Frequency Bin [Hz]")
-    ax.set_ylabel("Amplitude")
-    ax.set_xlim(0, nyquist_frequency)
-    return ax.figure, ax
-
-
-def plot_periodogram(
-    data: np.ndarray,
-    freq: np.ndarray,
-    nyquist_frequency: float,
-    ax=None,
-    **kwargs,
-):
-    if ax == None:
-        fig, ax = plt.subplots()
-
-    ax.loglog(freq, np.abs(data) ** 2, **kwargs)
-    flow = np.min(np.abs(freq))
-    ax.set_xlabel("Frequency [Hz]")
-    ax.set_ylabel("Periodigram")
-    # ax.set_xlim(left=flow, right=nyquist_frequency / 2)
-    return ax.figure, ax
+    # make yticks only use 2 digits, and use only 3 ticks
+    for ax in axs:
+        ax.set_xlabel(r"$t$ [days]")
+        ax.xaxis.set_major_locator(plt.MaxNLocator(nbins=3))
+        ax.yaxis.set_major_locator(plt.MaxNLocator(nbins=4, prune='both'))
+        ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+    return fig, axs
 
 
-def plot_timeseries(
-    data: np.ndarray, time: np.ndarray, ax=None, **kwargs
-) -> Tuple[plt.Figure, plt.Axes]:
-    """Custom method."""
-    if ax == None:
-        fig, ax = plt.subplots()
-    ax.plot(time, data, **kwargs)
-
-    ax.set_ylabel("Amplitude")
-    ax.set_xlim(left=time[0], right=time[-1])
-
-    _fmt_time_axis(time, ax)
-
-    return ax.figure, ax
-
-
-def plot_spectrogram(
-    timeseries_data: np.ndarray,
-    fs: float,
-    ax=None,
-    spec_kwargs={},
-    plot_kwargs={},
-) -> Tuple[plt.Figure, plt.Axes]:
-    f, t, Sxx = spectrogram(timeseries_data, fs=fs, **spec_kwargs)
-    if ax == None:
-        fig, ax = plt.subplots()
-
-    if "cmap" not in plot_kwargs:
-        plot_kwargs["cmap"] = "Reds"
-
-    cm = ax.pcolormesh(t, f, Sxx, shading="nearest", **plot_kwargs)
-
-    _fmt_time_axis(t, ax)
-
-    ax.set_ylabel("Frequency [Hz]")
-    ax.set_ylim(top=fs / 2.0)
-    cbar = plt.colorbar(cm, ax=ax)
-    cbar.set_label("Spectrogram Amplitude")
-    return ax.figure, ax
-
-
-def _fmt_time_axis(t, axes, t0=None, tmax=None):
-    if t[-1] > DAY_S:  # If time goes beyond a day
-        axes.xaxis.set_major_formatter(
-            plt.FuncFormatter(lambda x, _: f"{x / DAY_S:.1f}")
-        )
-        axes.set_xlabel("Time [days]")
-    elif t[-1] > HOUR_S:  # If time goes beyond an hour
-        axes.xaxis.set_major_formatter(
-            plt.FuncFormatter(lambda x, _: f"{x / HOUR_S:.1f}")
-        )
-        axes.set_xlabel("Time [hr]")
-    elif t[-1] > MIN_S:  # If time goes beyond a minute
-        axes.xaxis.set_major_formatter(
-            plt.FuncFormatter(lambda x, _: f"{x / MIN_S:.1f}")
-        )
-        axes.set_xlabel("Time [min]")
-    else:
-        axes.set_xlabel("Time [s]")
-    t0 = t[0] if t0 is None else t0
-    tmax = t[-1] if tmax is None else tmax
-    axes.set_xlim(t0, tmax)
+# data = load_data()
+fig, ax = generate_plot(data)
+plt.tight_layout()
