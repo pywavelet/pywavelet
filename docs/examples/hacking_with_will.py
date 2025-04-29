ed1d8f3a366401202b96d020730363a23d843bd6
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-04-28 22:19:41 -0500

    fix animation

--- docs/examples/hacking_with_will.py
+++ docs/examples/hacking_with_will.py
@@ -1,9 +1,13 @@
 import numpy as np
-from numpy import fft
 from scipy.special import betainc
 from scipy.signal import chirp
-from typing import List, Tuple
 import jax
+import matplotlib.pyplot as plt
+import jax.numpy as jnp
+from matplotlib.colors import TwoSlopeNorm
+import glob
+import os
+from PIL import Image
 
 jax.config.update('jax_enable_x64', True)
 
@@ -15,7 +19,6 @@ FREQ_RANGE = [20, 100]
 
 
 def omega(Nf: int, Nt: int):
-    """Get the angular frequencies of the time domain signal."""
     df = 2 * np.pi / (Nf * Nt)
     return df * np.arange(-Nt // 2, Nt // 2, dtype=np.float64)
 
@@ -23,10 +26,7 @@ def omega(Nf: int, Nt: int):
 def phitilde_vec_norm(
         Nf: int, Nt: int, d: float
 ):
-    """Normalize phitilde for inverse frequency domain transform."""
-    omegas = omega(Nf, Nt)
-    _phi_t = _phitilde_vec(omegas, Nf, d) * np.sqrt(np.pi)
-    return np.array(_phi_t)
+    return _phitilde_vec(omega(Nf, Nt), Nf, d) * np.sqrt(np.pi)
 
 
 def phi_vec(
@@ -34,7 +34,6 @@ def phi_vec(
 ):
     """get time domain phi as fourier transform of _phitilde_vec
     q: number of Nf bins over which the window extends?
-
     """
     insDOM = 1.0 / np.sqrt(np.pi / Nf)
     K = q * 2 * Nf
@@ -69,29 +68,7 @@ def _phitilde_vec(
         omega, Nf: int, d: float = 4.0
 ):
     """Compute phi_tilde(omega_i) array, nx is filter steepness, defaults to 4.
-
-    Eq 11 of https://arxiv.org/pdf/2009.00043.pdf (Cornish et al. 2020)
-
-    phi(omega_i) =
-        1/sqrt(2π∆F) if |omega_i| < A
-        1/sqrt(2π∆F) cos(nu_d π/2 * |omega|-A / B) if A < |omega_i| < A + B
-
-    Where nu_d = normalized incomplete beta function
-
-    Parameters
-    ----------
-    ω : xp.ndarray
-        Array of angular frequencies
-    Nf : int
-        Number of frequency bins
-    d : float, optional
-        Number of standard deviations for the gaussian wavelet, by default 4.
-
-    Returns
-    -------
-    xp.ndarray
-        Array of phi_tilde(omega_i) values
-
+    Eq 11 of Cornish '20
     """
     dF = 1.0 / (2 * Nf)  # NOTE: missing 1/dt?
     dOmega = 2 * np.pi * dF  # Near Eq 10 # 2 pi times DF
@@ -103,161 +80,20 @@ def _phitilde_vec(
         raise ValueError("B must be greater than 0")
 
     phi = np.zeros(omega.size, dtype=np.float64)
-    mask = (A <= np.abs(omega)) & (np.abs(omega) < A + B)  # Minor changes
-    vd = (np.pi / 2.0) * _nu_d(omega[mask], A, B, d=d)  # different from paper
+    mask = (A <= np.abs(omega)) & (np.abs(omega) < A + B)
+    vd = (np.pi / 2.0) * betainc(d, d, (np.abs(omega)[mask] - A) / B)
     phi[mask] = inverse_sqrt_dOmega * np.cos(vd)
     phi[np.abs(omega) < A] = inverse_sqrt_dOmega
     return phi
 
 
-def _nu_d(
-        omega, A: float, B: float, d: float = 4.0
-):
-    """Compute the normalized incomplete beta function.
-
-    Parameters
-    ----------
-    omega : np.ndarray
-        Array of angular frequencies
-    A : float
-        Lower bound for the beta function
-    B : float
-        Upper bound for the beta function
-    d : float, optional
-        Controlls the 'steepness'' -- 100 --> sqr wave, lower --> smoother roll
-
-
-    Returns
-    -------
-    np.ndarray
-        Array of ν_d values
-
-    scipy.special.betainc
-    https://docs.scipy.org/doc/scipy-1.7.1/reference/reference/generated/scipy.special.betainc.html
-
-    """
-    x = (np.abs(omega) - A) / B
-    return betainc(d, d, x)
-
-
-def simulate_data(Nf):
-    # assert Nf is power of 2
-    assert Nf & (Nf - 1) == 0, "Nf must be a power of 2"
-    fs = 512
-    dt = 1 / fs
-    Nt = Nf
-    mult = 16
-    nx = 4.0
-    ND = Nt * Nf
-    t = np.arange(0, ND) * dt
-    y = chirp(t, f0=FREQ_RANGE[0], f1=FREQ_RANGE[1], t1=t[-1], method="quadratic")
-    phif = phitilde_vec_norm(Nf, Nt, d=nx)
-    yf = fft.fft(y)[:ND // 2 + 1]
-
-    return yf, phif
-
-
-import matplotlib.pyplot as plt
-import jax
-import jax.numpy as jnp
-
-#
-# # collect plotting data
-# Nf = Nt = 2 ** 6
-# yf, phif = simulate_data(Nf)
-# wave = transform_wavelet_freq_helper_numba(yf, Nf, Nt, phif)
-# jax_yf = jnp.array(yf)
-# jax_phif = jnp.array(phif)
-# wave_jax = transform_wavelet_freq_helper_JAX(jax_yf, Nf, Nt, jax_phif)
-# nplots = 3
-#
-# print(f"Phif shape {phif.shape}")
-# print(f"data shape {jax_yf.shape}")
-#
-#
-# def xn_i(m: int, data: jnp.ndarray, Nf: int, Nt: int, phif: jnp.ndarray) -> jnp.ndarray:
-#     xni = jnp.fft.ifft(jnp.roll(data, - (m * Nt // 2))[:(Nt // 2) + 1] * phif)
-#     # print(xni.shape)
-#     return xni
-#
-#
-# def transform_conv(data: jnp.ndarray, Nf: int, Nt: int, phif: jnp.ndarray) -> jnp.ndarray:
-#     # fftshifft data to match up with Neil Eq 17
-#     data_shifted = jnp.fft.fftshift(data)
-#
-#     xn = jnp.array([
-#         xn_i(i, data_shifted, Nf, Nt, phif)  for i in range(Nf + 1)
-#     ])
-#
-#
-#
-#     #
-#     #
-#     # wave = jax.lax.fori_loop(
-#     #     0, Nt,
-#     #     lambda i, accum: jnp.stack((accum, xn_i(i, data, Nf, Nt, phif)), 1),
-#     #     jnp.zeros((Nt, 1), dtype=jnp.complex64)
-#     # )
-#     return wave[:, 1:]
-#
-#
-# wave_conv = transform_conv(jax_yf, Nf, Nt, jax_phif)
-#
-# # render plot
-# fig, ax = plt.subplots(1, nplots, figsize=(5, 5), sharex=True, sharey=True)
-# ax[0].imshow(np.abs(np.rot90(wave)))
-# ax[0].set_title("Numba")
-# ax[1].imshow(np.abs(np.rot90(wave_jax)))
-# ax[1].set_title("Jax")
-# ax[2].imshow(np.abs(np.rot90(wave_conv)))
-# ax[2].set_title("JaxCONV")
-#
-# for a in ax:
-#     a.set_xticks([])
-#     a.set_yticks([])
-#     a.set_ylim(120, 70)
-# plt.tight_layout()
-# plt.show()
-
-Nf = 32
-Nt = Nf
-nx = 4.0
-
-
-def simulate_data(Nf):
-    # assert Nf is power of 2
-    assert Nf & (Nf - 1) == 0, "Nf must be a power of 2"
-    fs = 512
-    dt = 1 / fs
-    Nt = Nf
-    mult = 16
-    nx = 1
-    ND = Nt * Nf
-    t = np.arange(0, ND) * dt
-    y = chirp(t, f0=FREQ_RANGE[0], f1=FREQ_RANGE[1], t1=t[-1], method="quadratic")
-    phif = phitilde_vec_norm(Nf, Nt, d=nx)
-    phit = phi_vec(Nf, d=nx)
-    yf = fft.fft(y)
-    return y, yf, phif, phit
-
-
-y, yf, phif, phit = simulate_data(Nf)
-# wave = transform_wavelet_freq_helper_numba(yf, Nf, Nt, phif)
-jax_yf = jnp.array(yf)
-jax_phif = jnp.array(phif)
-
-plt.plot(phitilde_vec_norm(Nf, Nt, d=1), label='d=1')
-plt.plot(phitilde_vec_norm(Nf, Nt, d=4), label='d=4')
-plt.plot(phitilde_vec_norm(Nf, Nt, d=100), label='d=100')
-plt.legend()
-plt.show()
-
-
-def xtilden_i(m: int, data: jnp.ndarray, Nf: int, Nt: int, phif: jnp.ndarray) -> jnp.ndarray:
-    i0 = data.shape[0] // 2 + 1
-    Ntby2 = Nt // 2
+def xtilde_i(m: int, data: jnp.ndarray, Nf: int, Nt: int, phif: jnp.ndarray) -> jnp.ndarray:
+    # i0 = data.shape[0] // 2 + 1
     # i0 is the center of the data -- used
-    xni = jnp.fft.ifft(jnp.roll(data, - (m * Ntby2))[i0 - (Ntby2):i0 + (Ntby2)] * phif)
+    i0 = (Nt * Nf) // 2 + 1
+    j0 = i0 - (Nt // 2)
+    j1 = i0 + (Nt // 2)
+    xni = jnp.fft.ifft(jnp.roll(data, - (m * Nt // 2))[j0: j1] * phif)
     return xni
 
 
@@ -277,14 +113,150 @@ def freq_to_wdm(data: jnp.ndarray, Nf: int, Nt: int, phif: jnp.ndarray) -> jnp.n
     # fftshifft data to match up with Neil Eq 17
     data_shifted = jnp.fft.fftshift(data)
     output = jnp.zeros((Nf, Nt), dtype=jnp.complex128)
+
+    # v1 = [xtilde_i(i, data_shifted, Nf, Nt, phif) for i in range(Nt)]
+
     return (jax.lax.fori_loop(
         0, Nt,
-        lambda i, output: output.at[:, i].set(xtilden_i(i, data_shifted, Nf, Nt, phif)),
+        lambda i, output: output.at[:, i].set(xtilde_i(i, data_shifted, Nf, Nt, phif)),
         output
     ) * Cnm_matrix(Nf, Nt)).real * jnp.sqrt(2.0)
 
 
+Nf = 32
+Nt = Nf
+nx = 4.0
 
+assert Nf & (Nf - 1) == 0, "Nf must be a power of 2"
+fs = 256
+dt = 1 / fs
+mult = 16
+nx = 1
+ND = Nt * Nf
+t = np.arange(0, ND) * dt
+f = np.fft.rfftfreq(ND, dt)
+T = ND * dt
+y = chirp(t, f0=FREQ_RANGE[0], f1=FREQ_RANGE[1], t1=t[-1], method="quadratic")
+phif = jnp.array(phitilde_vec_norm(Nf, Nt, d=nx))
+phit = phi_vec(Nf, d=nx)
+yf = jnp.fft.fft(y)
+
+# wave = transform_wavelet_freq_helper_numba(yf, Nf, Nt, phif)
+yf = jnp.array(yf)
+jax_phif = jnp.array(phif)
+
+plt.plot(phitilde_vec_norm(Nf, Nt, d=1), label='d=1')
+plt.plot(phitilde_vec_norm(Nf, Nt, d=4), label='d=4')
+plt.plot(phitilde_vec_norm(Nf, Nt, d=100), label='d=100')
+plt.legend()
+plt.show()
+
+wave = freq_to_wdm(yf, Nf, Nt, jax_phif)
+delta_T = T / Nt
+delta_F = 1 / (2 * delta_T)
+t_bins = np.arange(0, Nt) * delta_T
+f_bins = np.arange(0, Nf) * delta_F
+
+fig, axes = plt.subplots(2, 1, figsize=(4, 6))
+axes[0].semilogy(f, (np.abs(yf) ** 2)[:ND // 2 + 1], label="FFT")
+axes[0].set_xlabel("Frequency (Hz)", fontsize=14)
+axes[0].set_ylabel("Power", fontsize=14)
+axes[0].set_xlim(10, 100)
+axes[0].set_ylim(bottom=10)
+
+im = axes[1].imshow(
+    np.abs((wave.T)),
+    aspect="auto",
+    extent=[t_bins[0], t_bins[-1], f_bins[0], f_bins[-1]],
+    origin="lower",
+    interpolation="nearest",
+)
+axes[1].set_xlabel("Time (s)", fontsize=14)
+axes[1].set_ylabel("Frequency (Hz)", fontsize=14)
+plt.tight_layout()
+
+fig.subplots_adjust(hspace=0.5)
+plt.show()
+
+import os
+
+plt_dir = "wdm_plots"
+# make
+os.makedirs(plt_dir, exist_ok=True)
+
+f = np.fft.fftfreq(ND, dt)
+f = np.fft.fftshift(f)
+d = jnp.fft.fftshift(yf)
+output = jnp.zeros((Nf, Nt), dtype=jnp.complex128)
+i0 = (Nt * Nf) // 2 + 1
+j0 = i0 - (Nt // 2)
+j1 = i0 + (Nt // 2)
+min_wnm, max_wnm = np.min(wave), np.max(wave)
+
+for m in range(Nt):
+    rolled_d = jnp.roll(d, - (m * Nt // 2))
+    xni = jnp.fft.ifft(rolled_d[j0: j1] * phif)
+
+    fig = plt.figure(figsize=(5, 6))
+    gs = fig.add_gridspec(2, 2)
+
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax2 = fig.add_subplot(gs[0, 1])
+    ax3 = fig.add_subplot(gs[1, :])
+
+    ax1.plot(f, np.abs(d) ** 2, color='tab:gray', alpha=0.2)
+    ax1.plot(f, np.abs(rolled_d) ** 2, color='tab:gray', label='Roll(y)')
+    ax1.fill_between(np.arange(-Nt // 2, Nt / 2), max(np.abs(d) ** 2) * phif / max(phif), alpha=0.5, color='tab:orange',
+                     label='Window')
+    ax1.legend(loc='upper left', frameon=False)
+    ax1.set_ylim(bottom=0)
+    ax2.plot(t_bins, xni)
+    ax2.set_ylim(min_wnm, max_wnm)
+
+    ax1.set_xlabel('Freq [Hz]')
+    ax1.set_ylabel('Power')
+
+    ax2.set_xlabel('Time [s]')
+    ax2.set_ylabel('Wnm')
+
+    wdm_m = np.array(wave)
+    wdm_m[:, m + 1:] = 0
+    # cmap of 'bwr' with vcenter=0
+
+    curr_min = min(wdm_m.min(), -1)
+    curr_max = max(wdm_m.max(), 1)
+
+    norm = TwoSlopeNorm(0, curr_min, curr_max)
+    im = ax3.imshow(wdm_m.T, aspect="auto", extent=[t_bins[0], t_bins[-1], f_bins[0], f_bins[-1]],
+                    origin="lower", interpolation="nearest", norm=norm, cmap='bwr')
+    ax3.set_xlabel("Time [s]", fontsize=14)
+    ax3.set_ylabel("Freq [Hz]", fontsize=14)
+    # add colorbar and label it Wnm
+    cbar = fig.colorbar(im, ax=ax3)
+    cbar.set_label("Wnm", fontsize=14)
+    plt.tight_layout()
+    plt.savefig(f"{plt_dir}/wdm_{m:02d}.png")
+
+
+# make gif from images (sorted by name)
+
+
+def make_gif_from_images(image_dir, gif_name, duration=3):
+    images = []
+    files = glob.glob(os.path.join(image_dir, '*.png'))
+    files = sorted(files, key=lambda x: int(os.path.basename(x).split('_')[1].split('.')[0]))
+    for filename in files:
+        images.append(Image.open(filename))
+    images[0].save(
+        gif_name,
+        save_all=True,
+        append_images=images[1:],
+        duration=duration * 1000,  # Convert seconds to milliseconds
+        loop=0
+    )
+
+
+make_gif_from_images(plt_dir, "wdm.gif", duration=0.5)
 
 #
 #
@@ -328,23 +300,5 @@ def freq_to_wdm(data: jnp.ndarray, Nf: int, Nt: int, phif: jnp.ndarray) -> jnp.n
 #
 # def
 
-wave = freq_to_wdm(jax_yf, Nf, Nt, jax_phif)
-# wave2 = freq_to_wdm(jax_yf, Nf, Nt, jax_phif)
 
-T = 4.0
-delta_T = T / Nt
-delta_F = 1 / (2 * delta_T)
-t_bins = np.arange(0, Nt) * delta_T
-f_bins = np.arange(0, Nf) * delta_F
-
-
-im = plt.imshow(
-    np.abs((wave.T)),
-    aspect="auto",
-    extent=[t_bins[0], t_bins[-1], f_bins[0], f_bins[-1]],
-    origin="lower",
-    interpolation="nearest",
-)
-plt.xlabel("Time (s)")
-plt.ylabel("Frequency (Hz)")
-plt.show()
+# wave2 = freq_to_wdm(yf, Nf, Nt, jax_phif)
